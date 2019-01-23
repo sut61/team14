ad9c992ac5e9465be0fdde877fd4f57b59c76ab8
thanyn <tthanyn@gmail.com>
2019-01-23 23:32:40 +0700

    add Register system Entity - close #33

---
+++ backend/src/main/java/sut/se/g14/entity/Country.java
@@ -0,0 +1,22 @@
+package sut.se.g14.entity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Country {
+    @Id
+    @SequenceGenerator(name = "country_seq",sequenceName = "country_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "country_seq")
+    @Column(name = "COUNTRY_ID")
+
+    private @NonNull Long countryID;
+    private @NonNull String countryName;
+
+
+}
